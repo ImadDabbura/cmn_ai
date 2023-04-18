ff0a3d54261a49e38460b835e3dfb16917beaad2
ImadDabbura <imad.dabbura@hotmail.com>
2023-04-18 17:18:16 -0500

    feat: Add clean memory

--- cmn_ai/utils/utils.py
+++ cmn_ai/utils/utils.py
@@ -112,3 +112,39 @@ def set_seed(seed: int = 42, deterministic: bool = False) -> None:
     torch.manual_seed(seed)
     random.seed(seed)
     np.random.seed(seed)
+
+
+def clean_ipython_history():
+    # Code in this function mainly copied from IPython source
+    if "get_ipython" not in globals():
+        return
+    ip = get_ipython()  # noqa: F821
+    user_ns = ip.user_ns
+    ip.displayhook.flush()
+    pc = ip.displayhook.prompt_count + 1
+    for n in range(1, pc):
+        user_ns.pop("_i" + repr(n), None)
+    user_ns.update(dict(_i="", _ii="", _iii=""))
+    hm = ip.history_manager
+    hm.input_hist_parsed[:] = [""] * pc
+    hm.input_hist_raw[:] = [""] * pc
+    hm._i = hm._ii = hm._iii = hm._i00 = ""
+
+
+def clean_traceback():
+    # Very useful especially when traceback has big tensors
+    # attached to a traceback while the operation raised Exception
+    if hasattr(sys, "last_traceback"):
+        traceback.clear_frames(sys.last_traceback)
+        delattr(sys, "last_traceback")
+    if hasattr(sys, "last_type"):
+        delattr(sys, "last_type")
+    if hasattr(sys, "last_value"):
+        delattr(sys, "last_value")
+
+
+def clean_memory():
+    clean_traceback()
+    clean_ipython_history()
+    gc.collect()
+    torch.cuda.empty_cache()
