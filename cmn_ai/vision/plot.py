9e140aca008c9f744c1ca6b8c7e459fccbe34896
ImadDabbura <imad.dabbura@hotmail.com>
2023-04-24 13:17:22 -0500

    feat: Add subplots

--- cmn_ai/vision/plot.py
+++ cmn_ai/vision/plot.py
@@ -56,3 +56,45 @@ def show_image(
     if noframe:
         ax.axis("off")
     return ax
+
+
+@fc.delegates(plt.subplots, keep=True)
+def subplots(
+    nrows: int = 1,
+    ncols: int = 1,
+    figsize: tuple = None,
+    imsize: int = 3,
+    suptitle: str = None,
+    **kwargs,
+) -> tuple[Figure, Axes]:
+    """
+    A figure and set of subplots to display images of `imsize` inches.
+
+    Parameters
+    ----------
+    nrows : int, default=1
+        Number of rows in returned axes grid.
+    ncols : int, default=1
+        Number of columns in returned axes grid.
+    figsize : tuple, default=None
+        Width, height in inches of the returned Figure.
+    imsize : int, default=3
+        Size (in inches) of images that will be displayed in the returned figure.
+    suptitle : str, default=None
+        Title of the Figure.
+
+    Returns
+    -------
+    fig : Figure
+        Top level container for all axes.
+    ax : array of Axes
+        Array of axes.
+    """
+    if figsize is None:
+        figsize = (ncols * imsize, nrows * imsize)
+    fig, ax = plt.subplots(nrows, ncols, figsize=figsize, **kwargs)
+    if suptitle is not None:
+        fig.suptitle(suptitle)
+    if nrows * ncols == 1:
+        ax = np.array([ax])
+    return fig, ax
