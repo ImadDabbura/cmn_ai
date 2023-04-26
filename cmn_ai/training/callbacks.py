e1600dccd58b6fae7ed03d4f2c3dd317e7321bc4
ImadDabbura <imad.dabbura@hotmail.com>
2023-04-26 10:43:15 -0500

    feat: Add callback base class

--- cmn_ai/training/callbacks.py
+++ cmn_ai/training/callbacks.py
@@ -1,3 +1,14 @@
+import re
+import tempfile
+import time
+from functools import partial
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import torch
+from fastprogress.fastprogress import format_time, master_bar, progress_bar
+
+from ..utils.data import default_device, to_cpu, to_device
 from ..utils.utils import listify, setify
 
 
@@ -27,3 +38,40 @@ class CancelStepException(Exception):
 
 class CancelBackwardException(Exception):
     """Skip the backward pass and move to after_backward."""
+
+
+class Callback:
+    """Base class for all callbacks."""
+
+    order = 0
+
+    def set_learner(self, learner):
+        self.learner = learner
+
+    def __getattr__(self, k):
+        return getattr(self.learner, k)
+
+    @property
+    def name(self):
+        """
+        Returns the name of the callback after removing the word `callback`
+        and then convert it to snake (split words by underscores).
+        """
+        name = re.sub(r"Callback$", "", self.__class__.__name__)
+        return Callback.camel2snake(name or "callback")
+
+    def __call__(self, event_nm):
+        method = getattr(self, event_nm, None)
+        if method is not None:
+            method()
+
+    @staticmethod
+    def camel2snake(name):
+        """
+        Convert name of callback by inserting underscores between small and capital
+        letters. For example, `TestCallback` becomes `test_callback`.
+        """
+        pattern1 = re.compile("(.)([A-Z][a-z]+)")
+        pattern2 = re.compile("([a-z0-9])([A-Z])")
+        name = re.sub(pattern1, r"\1_\2", name)
+        return re.sub(pattern2, r"\1_\2", name).lower()
