f767cb58f222f07b8879fb82fa1b87be52e88b6b
ImadDabbura <imad.dabbura@hotmail.com>
2023-05-08 18:54:57 -0500

    feat: Add image list

--- cmn_ai/vision/data.py
+++ cmn_ai/vision/data.py
@@ -9,3 +9,41 @@ from ..utils.data import ItemList, get_files
 IMAGE_EXTENSIONS = [
     k for k, v in mimetypes.types_map.items() if v.startswith("image/")
 ]
+
+
+class ImageList(ItemList):
+    """
+    Build an image list from list of files in the `path` end with
+    extensions, optionally recursively.
+
+    Parameters
+    ----------
+    path : str | Path
+        Path for the root directory to search for files.
+    extensions : str | Iterable[str] | None, default=IMAGE_EXTENSIONS
+        Suffixes of filenames to look for.
+    include : Iterable[str] | None, default=None
+        Top-level Director(y|ies) under `path` to use to search for files.
+    recurse : bool, default=True
+        Whether to search subdirectories recursively.
+
+    Returns
+    -------
+    list[str]
+        List of filenames that ends with `extensions` under `path`.
+    """
+
+    @classmethod
+    def from_files(
+        cls,
+        path: str | Path,
+        extensions: Iterable[str] | str = IMAGE_EXTENSIONS,
+        include: Iterable[str] | None = None,
+        recurse: bool = True,
+        **kwargs
+    ):
+        return cls(get_files(path, extensions, include, recurse), **kwargs)
+
+    def get(self, item):
+        """Open an image using PIL."""
+        return PIL.Image.open(item)
