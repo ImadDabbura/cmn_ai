16c7ca65656832f08c8a063beefbc8600fb1e5f0
ImadDabbura <imad.dabbura@hotmail.com>
2023-05-19 08:38:17 -0500

    feat: Add non reduction loss

--- cmn_ai/callbacks/training.py
+++ cmn_ai/callbacks/training.py
@@ -24,7 +24,7 @@ import time
 from copy import copy
 from functools import partial
 from pathlib import Path
-from typing import Iterable
+from typing import Callable, Iterable
 
 import fastcore.all as fc
 import matplotlib.pyplot as plt
@@ -407,3 +407,58 @@ class MetricsCallback(Callback):
         self.all_metrics["loss"].update(
             to_cpu(self.learner.loss), weight=len(self.learner.xb[0])
         )
+
+
+class NoneReduce:
+    def __init__(self, loss_func: Callable):
+        """
+        Force non-reduction on the loss tensor so it can used later in methods
+        such as `Mixup` or `LabelSmoothing`.
+
+        Parameters
+        ----------
+        loss_func : Callable
+            Loss function.
+        """
+        self.loss_func = loss_func
+        self.old_reduction = None
+
+    def __enter__(self):
+        if hasattr(self.loss_func, "reduction"):
+            self.old_reduction = getattr(self.loss_func, "reduction")
+            setattr(self.loss_func, "reduction", "none")
+            return self.loss_func
+        else:
+            return partial(self.loss_func, reduction="none")
+
+    def __exit__(self, exc_type, exc_val, traceback):
+        if self.old_reduction is not None:
+            setattr(self.loss_func, "reduction", self.old_reduction)
+
+
+def reduce_loss(
+    loss: torch.Tensor, reduction: str | None = None
+) -> torch.Tensor:
+    """
+    Reduce the `loss` tensor using `reduction` method. If `reduction` is None,
+    returns the passed loss tensor.
+
+    Parameters
+    ----------
+    loss : torch.Tensor
+        Loss tensor.
+    reduction : str | None, default=None
+        Reduction applied to the loss tensor.
+
+    Returns
+    -------
+    torch.Tensor
+        Reduced loss tensor.
+    """
+    return (
+        loss.mean()
+        if reduction == "mean"
+        else loss.sum()
+        if reduction == "sum"
+        else loss
+    )
