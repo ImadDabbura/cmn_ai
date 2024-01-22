b9d2aee7aca4dc825fe3d9ca1e585bff06b3dd24
ImadDabbura <imad.dabbura@hotmail.com>
2024-01-22 17:38:26 -0600

    feat: Pass vocab to CategoryProcessor
    This change allow the user to pass vocab in the case it has to be
    ordered; otherwise, index for tokens in the vocab wouldn't follow any
    orders and will be assigned randomly.

--- cmn_ai/utils/processors.py
+++ cmn_ai/utils/processors.py
@@ -12,27 +12,42 @@ from .utils import uniqueify
 class Processor:
     """Base class for all processors."""
 
-    def process(self, item):
-        return item
-
 
 class CategoryProcessor(Processor):
     """
     Create a vocabulary from training data and use it to numericalize
     categories/text.
+
+    Attributes
+    ----------
+    vocab : Sequence[str]
+        Vocabulary used for numericalizing tokens.
+    otoi : Dict[str, int]
+        Mapping of tokens to their integer indices.
     """
 
-    def __init__(self) -> None:
-        self.vocab = None
+    def __init__(self, vocab: Sequence[str] | None = None) -> None:
+        """
+        Parameters
+        ----------
+        vocab : Sequence[str] | None, default=None
+            Vocabulary to use for numericalizing tokens.
+        """
+        self.vocab = vocab
+        self.otoi = (
+            None
+            if self.vocab is None
+            else {k: v for v, k in enumerate(self.vocab)}
+        )
 
     def __call__(self, items: Sequence[str] | str) -> list[int] | int:
         """
-        Create a vocabulary from items if it doesn't already exist and return
-        their numerical IDs.
+        Create a vocabulary from items if it doesn't already exist and
+        return their numerical IDs.
 
         Parameters
         ----------
-        items : Sequence[int] | int
+        items : Sequence[str] | str
             Data to numericalize.
 
         Returns
@@ -46,11 +61,37 @@ class CategoryProcessor(Processor):
         return self.process(items)
 
     def process(self, items: Iterable[str] | str) -> list[int] | int:
+        """
+        Numericalize item(s).
+
+        Parameters
+        ----------
+        items : Iterable[str] | str
+            Items to numericalize.
+
+        Returns
+        -------
+        list[int] | int
+            Numerical IDs of passed items.
+        """
         if isinstance(items, Iterable):
             return [self.otoi[item] for item in items]
         return self.otoi[items]
 
-    def deprocess(self, idxs: Iterable[str] | int) -> list[str] | str:
+    def deprocess(self, idxs: Iterable[int] | int) -> list[str] | str:
+        """
+        Denumericalize item(s) by converting IDs to actual tokens.
+
+        Parameters
+        ----------
+        idxs : Iterable[int] | int
+            IDs to denumricalize.
+
+        Returns
+        -------
+        list[str] | str
+            Tokens that correspond for each ID.
+        """
         if isinstance(idxs, Iterable):
             return [self.vocab[idx] for idx in idxs]
         return self.vocab[idxs]
