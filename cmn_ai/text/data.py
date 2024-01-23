162943f40f901ebab30774440cfd7b56f5e75a71
ImadDabbura <imad.dabbura@hotmail.com>
2024-01-22 18:00:45 -0600

    feat: Add encoding parameter to TextList

--- cmn_ai/text/data.py
+++ cmn_ai/text/data.py
@@ -9,24 +9,7 @@ from ..utils.data import ItemList, get_files
 class TextList(ItemList):
     """
     Build a text list from list of files in the `path` end with
-    extensions, optionally recursively.
-
-    Parameters
-    ----------
-    path : str | Path
-        Path for the root directory to search for files.
-    extensions : str | Iterable[str] | None, default=IMAGE_EXTENSIONS
-        Suffixes of filenames to look for.
-    include : Iterable[str] | None, default=None
-        Top-level Director(y|ies) under `path` to use to search for files.
-    recurse : bool, default=True
-        Whether to search subdirectories recursively.
-    tfms : Callable | None, default=None
-        Transformations to apply items before returning them.
-    Returns
-    -------
-    list[str]
-        List of filenames that ends with `extensions` under `path`.
+    `extensions`, optionally recursively.
     """
 
     @classmethod
@@ -37,19 +20,35 @@ class TextList(ItemList):
         include: Iterable[str] | None = None,
         recurse: bool = True,
         tfms: Callable | None = None,
-        **kwargs,
+        encoding="utf8",
     ) -> TextList:
         """
-        Build an text list from list of files in the `path` end with
-        extensions, optionally recursively.
+        Parameters
+        ----------
+        path : str | Path
+            Path for the root directory to search for files.
+        extensions : str | Iterable[str] | None, default=IMAGE_EXTENSIONS
+            Suffixes of filenames to look for.
+        include : Iterable[str] | None, default=None
+            Top-level Director(y|ies) under `path` to use to search for
+            files.
+        recurse : bool, default=True
+            Whether to search subdirectories recursively.
+        tfms : Callable | None, default=None
+            Transformations to apply items before returning them.
+        encoding : str, default=utf8
+            Name of encoding used to decode files.
         """
         return cls(
-            get_files(path, extensions, include, recurse), path, tfms, **kwargs
+            get_files(path, extensions, include, recurse),
+            path,
+            tfms,
+            encoding=encoding,
         )
 
     def get(self, i):
         """Returns text in the file as string if `i` is path to a file."""
         if isinstance(i, Path):
-            with open(i, encoding="utf8") as f:
+            with open(i, encoding=self.encoding) as f:
                 return f.read()
         return i
